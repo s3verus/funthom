e9700c5c73ecf3f696ada77b91cc9bf6ffc9ebb5
s3verus <shervin.abdollahi18@gmail.com>
2018-12-20 22:45:39 +0330

    improved...

--- funthom.py
+++ funthom.py
@@ -7,6 +7,7 @@
 #i improve it very soon , please wait...
 
 import os
+
 print("funthom\ncreated by S3verus\n")
 print(" 1.change local ip \n 2.change mac address \n 3.local scan \n 4.port scan \n 5.ping ip \n \n 00.install dpndns \n 99.exit \n")
 x=input("choose number:\n")
@@ -16,27 +17,27 @@ if x == 1 :
     newip=input("enter new ip: (192.168.1.2)\n")
     newmask=input("new netmask: (255.255.255.0)\n")
     intf=input("enter your interface: (eth0/wlan0)\n")
-    os.system("ifconfig eth0 192.168.1.2 netmask 255.255.255.0")
+    os.system("ifconfig %s 192.168.1.2 netmask 255.255.255.0" %intf)
 else:
     if x == 2:
         newmac=input("enter new mac address: (00:00:00:00:00:02)\n")
         intf=input("enter your interface: (eth0/wlan0)\n")
-        os.system("ifconfig eth0 down")
-        os.system("ifconfig eth0 hw ether 00:00:00:00:00:02")
-        os.system("ifconfig eth0 up")
+        os.system("ifconfig %s down" %intf)
+        os.system("ifconfig %s hw ether 00:00:00:00:00:02" %intf)
+        os.system("ifconfig %s up" %intf)
     else:
         if x == 3:
             ip=input("enter ip with type: (192.168.1.0/24) \n")
-            os.system("nmap -sP 192.168.1.0/24")
+            os.system("nmap -sP %s" %ip)
         else:
             if x == 4:
                 ip=input("enter ip to scan: (192.168.1.1)\n")
                 iprng=input("enter range of ports: (for EX: 1-6000)\n")
-                os.system("nmap -p 1-6000 192.168.1.1")
+                os.system("nmap -p %s 192.168.1.1" %iprng)
             else:
                 if x == 5:
                     ip=input("enter ip to ping: (192.168.1.1)\n")
-                    os.system("ping 192.168.1.1")
+                    os.system("ping %s" %ip)
                 else:
                     if x == 99:
                         print("tnx for download...")
